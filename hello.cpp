7286b006b76c4bc4104b554050cb4427abf21892
Sworce <Sworce>
2019-09-04 16:13:00 -0700

    Changed the greeting

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,11 @@
+//Author: Tommy Luo
 #include <iostream>
 #include <string>
 
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout << "Hi, my name is Tommy. What's yours?" << std::endl;
     std::cin >> input;
-    std::cout << "Hello, " << input << std::endl;
+    std::cout << "Hello " <<  input << std::endl;
     return 0;
 }
